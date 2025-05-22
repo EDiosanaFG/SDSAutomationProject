2a94677d882f38cb23f70af6f5eab3d4c0ea48d3
Ethan Diosana <ediosana@factorsgroup.com>
2025-05-22 09:38:16 -0700

    Created basic server

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("running");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const config_json_1 = require("./json/config.json");
+const PORT = config_json_1.server.port;
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.send(`Running on port ${PORT}`);
+});
+app.post('/extractSDSData', (req, res) => {
+    // TODO
+});
+app.listen(() => {
+    console.log(`Running on port ${PORT}`);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("running");
+import express, { Request, Response } from 'express';
+import { server } from './json/config.json';
+
+const PORT = server.port;
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(`Running on port ${PORT}`);
+});
+
+app.post('/extractSDSData', (req: Request, res: Response) => {
+    // TODO
+});
+
+app.listen(() => {
+    console.log(`Running on port ${PORT}`);
+});
